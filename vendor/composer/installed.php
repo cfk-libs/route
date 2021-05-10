51217c59d5d7039d0ba5af3c5a36bbeb62efdd73
Cemre Fatih Karakulak <cradexco@gmail.com>
2021-05-10 23:16:14 +0300

    carry

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '84c40c0d2c60bc4ff148326f30c1da5887aa5232',
+    'name' => 'sellony/router',
+  ),
+  'versions' => 
+  array (
+    'sellony/router' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '84c40c0d2c60bc4ff148326f30c1da5887aa5232',
+    ),
+  ),
+);
